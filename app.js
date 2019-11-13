1b9418da6b57c94d719db13842036d3b0ebd1689
Fahad <45518093+mfahadqureshi786@users.noreply.github.com>
2019-11-13 20:39:22 +0500

    added sessions

--- app.js
+++ app.js
@@ -1,10 +1,44 @@
 const exp=require('express');
-const app=exp();
+
+const uuid = require('uuid/v4');
 var bodyParser = require('body-parser');
+var session=require('express-session');
+var MySQLStore = require('express-mysql-session')(session);
+var cookieParser = require('cookie-parser')
+ 
+
+var mysql      = require('mysql');
+var connection = mysql.createConnection({
+  host     : 'localhost',
+  user     : 'root',
+  password : '',
+  database : 'users',
+  clearExpired: true //auto remove expired session tuples from db 
+});
+
+connection.connect();
+var sessionStore = new MySQLStore({}/* session store options */, connection);
+var user='fahad';
+var pass='123456';
+module.exports.connection=connection;
+var UserPrototype=require("./controllers/user.js").UserPrototype;
+const app=exp();
 
 app.set('view engine', 'ejs');
 app.use(exp.static('public'));
 app.use(bodyParser.urlencoded({ extended: true }));
+app.use(session({
+	genid: (req) => {
+    console.log('Creating session Id')
+    return uuid(); // use UUIDs for session IDs
+  },
+	name:'Session-Cookie',
+  secret: 'keyboard cat',
+  resave: false,
+  saveUninitialized: false,
+  store:sessionStore,
+  cookie: {secure: false,maxAge: 5*60*1000 }//session cookie expire after 5 mins
+}));
 
 
 app.get('/',(req,res)=>{
@@ -13,10 +47,27 @@ res.render('home_copy.ejs');
 });
 app.get('/login',(req,res)=>{
 console.log('On login route');
+if(req.session.customer_id==undefined)
+{console.log("On login page");
 res.render('loginSignup_copy.ejs');
+}
+else
+{console.log("signed in user on login page");
+res.redirect('/');
+}
+
 });
 
-
+app.post('/login',(req,res)=>{
+UserPrototype.exist(req.body.user).then((val)=>{
+		UserPrototype.findUserDetails(req.body.user,req.body.pass).then((info)=>{
+			req.session.customer_id=info.id;
+			res.redirect('/');
+		});
+	}).catch((msg)=>{console.log(msg);
+res.redirect('/');
+	});
+})
 
 
 
