099a62a90933adbefd939753ebc9769bd0232f5e
Fahad <45518093+mfahadqureshi786@users.noreply.github.com>
2019-11-20 23:30:47 +0500

    data from db to searchflghts partially

--- app.js
+++ app.js
@@ -12,7 +12,7 @@ var connection = mysql.createConnection({
   host     : 'localhost',
   user     : 'root',
   password : '',
-  database : 'users',
+  database : 'airlinems',
   clearExpired: true //auto remove expired session tuples from db 
 });
 
@@ -108,23 +108,30 @@ res.redirect('/');
 
 
 
-function Flight(flightNo,departure,arrival,duration,price){
-	this.flightNo=flightNo;
-	this.departure=departure;
-	this.arrival=arrival;
-	this.duration=duration;
+function Flight(FLIGHT_int,SOURCE,DESTINATION,DEPARTURE_Date,price,AIRPLANE_ID){
+	this.FLIGHT_int=FLIGHT_int;
+	this.SOURCE=SOURCE;
+	this.DESTINATION=DESTINATION;
+	this.DEPARTURE_Date=DEPARTURE_Date;
+	this.AIRPLANE_ID=AIRPLANE_ID;
 	this.price=price;
 }
-var flightObj=new Flight('pk301','pakistan 1:30pm','Canada 3:30pm','6:00pm','Pkr100,0000');
-var flightArr=[flightObj];
-flightArr.push(flightObj);
-flightArr.push(flightObj);
-flightArr.push(flightObj);
 
+app.get('/SearchFlights',(req,res)=>{
+UserPrototype.searchFlights("islamabad","ontario","2019-11-20").then((result)=>{
+	var flightArr_local=[];
+	for (var i = 0; i < result.length; i++) {
+	var flightObj_local=new Flight(result[i].FLIGHT_int,result[i].SOURCE,result[i].DESTINATION,result[i].DEPARTURE_Date,result[i].AIRPLANE_ID,
+		result[i].price);
+	flightArr_local.push(flightObj_local);
+	}
+	res.render('test_search.ejs',{ssData:flightArr_local});
+console.log(result);
+}).catch((error)=>{
+	res.redirect('/');
+	console.log(error);
+});
 
-app.get('/searchFlights',(req,res)=>{
-	console.log(req.query.to);
-res.render('test_search.ejs',{ssData:flightArr});
 });
 
 function flightReq(flightNumber,source,destination,startTime,stay){
